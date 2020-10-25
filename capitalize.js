7afe232cc8c2f7b8b224e44880e6903d83e86996
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-24 20:10:47 -0700

    add html and js file for capitalize exercise

---
+++ capitalize.js
@@ -0,0 +1,7 @@
+// DEFINE YOUR FUNCTION BELOW:
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+console.log(capitalize('eggplant'));
+console.log(capitalize('pamplemousse'));
+console.log(capitalize('squid'))
